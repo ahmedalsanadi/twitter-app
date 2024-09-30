b5ba27aade0eddd902d789e9f110e7f99a43748a
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2024-09-30 10:03:21 +0300

    Feat : Added toggle to Register Form

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -1,10 +1,12 @@
 import React, { useCallback, useState } from 'react';
 import useLoginModal from '@/hooks/useLoginModal';
+import useRegisterModal from '@/hooks/useRegisterModal';
 import Input  from '@/components/Input';
 import Modal from '../Modal';
 
 const LoginModal = () => {
 	const loginModal = useLoginModal();
+	const registerModal = useRegisterModal();
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 	const [isLoading, setIsLoading] = useState(false);
@@ -22,6 +24,15 @@ const LoginModal = () => {
 			setIsLoading(false);
 		}
 	}, [loginModal]);
+
+	const onToggle = useCallback(() => {
+        if (isLoading) {
+            return;
+        }
+		loginModal.onClose();
+        registerModal.onOpen();
+    }, [isLoading, registerModal, loginModal]);
+
 
 	const bodyContent = (
 		<div className="flex flex-col gap-4">
@@ -41,6 +52,20 @@ const LoginModal = () => {
 			/>
 		</div>
 	);
+
+	const footerContent = (
+        <div className="text-neutral-400 text-center mt-4">
+            <p>
+                First time using Twitter? 
+                <span
+                    onClick={onToggle}
+                    className="text-white cursor-pointer hover:underline ml-1"
+                >
+                    Create an account 
+                </span>
+            </p>
+        </div>
+    );
 	return (
 		<Modal
 			disabled={isLoading}
@@ -50,6 +75,7 @@ const LoginModal = () => {
 			onClose={loginModal.onClose}
 			onSubmit={onSubmit}
 			body={bodyContent}  
+			footer={footerContent}
 		/>
 	);
 };
