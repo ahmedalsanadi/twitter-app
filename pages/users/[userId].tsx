b07ead664e43a6b52aef89f1ff33c1127599fb34
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2024-10-02 21:24:36 +0300

    feat: Add initial user profile page with dynamic user data
    - Created a new page at `/users/[userId].tsx` to display user-specific profiles.
    - Integrated `useRouter` to retrieve the `userId` from the URL and fetch user data using the `useUser` hook.
    - Implemented a loading state with a `ClipLoader` spinner to display while user data is being fetched.
    - Displayed the user's name dynamically in the `Header` component when data is loaded.
    - Added placeholder for the future `UserHero` component to display user details.

--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -1,6 +1,29 @@
+//pages/users/[userId].tsx
+import { useRouter } from "next/router"
+
+import useUser from "@/hooks/useUser"
+import Header from "@/components/Header"
+import { ClipLoader } from "react-spinners";
+
 const UserView = () => {
+    const router = useRouter();
+    const { userId } = router.query;
+
+    const { data: fetchedUser, isLoading } = useUser({ userId: userId as string })
+
+    if (isLoading || !fetchedUser) {
+        return (
+            <div className="flex justify-center items-center h-full">
+               <ClipLoader color="lightblue"  size={80}/>
+            </div>
+        )
+    }
+
   return (
-    <div>UserView</div>
+   <>
+    <Header showBackArrow label={fetchedUser?.name} />
+    {/* UserHero component */}
+   </>
   )
 }
 
